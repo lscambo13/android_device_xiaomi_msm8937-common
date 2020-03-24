f415a0ca83e2859713d8259e349e67c79ebbed5e
sairam1411 <majgaonkars3@gmail.com>
2020-03-24 05:33:15 +0000

    msm8937-common: Downgrade thermal

--- msm8937.mk
+++ msm8937.mk
@@ -376,8 +376,9 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-impl \
-    android.hardware.thermal@2.0-service
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8937
 
 PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
